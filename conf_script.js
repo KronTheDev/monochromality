8a615923c3d91dc3625add62f801687af0df2296
KronTheDev <165278219+KronTheDev@users.noreply.github.com>
2025-11-12 16:11:43 +0100

    .

--- conf_script.js
+++ conf_script.js
@@ -19,7 +19,26 @@ const templates = ["Test",
     ## Attachments
 
     **[Preferably Images; Rarely any other Files.]**`,
-"Template 2"];
+  `DELETE THIS PART
+    Replace all of the objects between brackets with your own Suggestion content, but do not touch any other part (especially parentheses).
+
+    # *[Suggestion Name] (Level Suggestion)*
+
+    ## Pitch:
+    ***[Short Paragraph of 4-5 lines describing the general feel and purpose of the Level]***
+
+    ## Notice:
+    - **[Argument 1]**
+    - **[Argument 2]**
+    - **[Argument 3]**
+    - **... up to 5 arguments to make your Suggestion attractive.**
+
+    ## Description:
+    *[Describe your Level as in-depth as possible]*
+
+    ## Attachments
+
+    **[Preferably Images; Rarely any other Files.]**`,];
 
 
 function copyClip (id) {
